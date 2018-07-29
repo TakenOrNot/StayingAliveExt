8c1f360d622663056977119d78bcd58f760913b5
gitgud <xdevzone@live.fr>
2018-07-29 17:06:39 +0200

    test

--- stayingalive.js
+++ stayingalive.js
@@ -21,7 +21,7 @@
     // cdtime = 60000*9;
     cdtime = 60000*8;
     window.idletimeelapsed = 0;
-    window.idletimecounterinterval = setInterval(idletimecounter, 1000);
+    //window.idletimecounterinterval = setInterval(idletimecounter, 1000);
     
     /* GUI */
     
@@ -164,11 +164,11 @@
                     console.log("v key pressed, show stay alive GUI");
                     $("#stayalivecontainer").css({display: "block"});
                     // TODO : start counting idle time
-                    // var idletimecounterinterval = setInterval(idletimecounter, 1000);
+                    var idletimecounterinterval = setInterval(idletimecounter, 1000);
                     // var idletimeelapsed = 0;
                     function idletimecounter() {
                         
-                        idletimeelapsed++ 
+                        window.idletimeelapsed++ 
                     }
                 }
             },2000);    
