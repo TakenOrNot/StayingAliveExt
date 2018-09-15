7df10715c13c5295de98d56d1ed9893c8455f9a3
gitgud <xdevzone@live.fr>
2018-09-15 12:20:25 +0200

    GUI

--- focus-test.js
+++ focus-test.js
@@ -58,12 +58,12 @@
             //$('body').removeClass('focusmode');
             $('#logosmall').toggle( "slide" );
             $('#scorebig').toggle( "slide" );
-            $('#roomnamecontainer').toggle( "slide" );
+            $('#roomnamecontainer').fadeToggle( "fast");
             $('#scoreboard').toggle( "slide" );
-            $('#sidebar').toggle( "slide" );
-            $('#menu').toggle( "slide" );
+            $('#sidebar').fadeToggle( "fast");
+            $('#menu').fadeToggle( "fast");
             $('#chatbox').toggle( "slide" );
-            $('#settings').toggle( "slide" );
+            $('#settings').fadeToggle( "fast");
             $("#focus").html('Focus Mode');
         }
     });
